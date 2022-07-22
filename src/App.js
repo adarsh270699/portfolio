df889e15cc1a6e0218e8467a6c4a921894258f15
adarsh270699 <poddaradarsh27@gmail.com>
2022-07-23 02:35:37 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <div className="h-full flex flex-wrap justify-center content-center">
         <div className='bg-white shadow-sm hover:bg-gray-600 hover:text-white hover:border-white  hover:border-2 transition duration-500 rounded-xl p-10 space-y-5'>
           <img className='h-52' src={img} alt='img'></img>
-          <h1 className='font-bold	text-3xl'>Site under construction!</h1>
+          <h1 className='font-bold text-3xl'>Site under construction!</h1>
         </div>
       </div>
     </div>
