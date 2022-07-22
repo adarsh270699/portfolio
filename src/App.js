ee732cb98964498a5144e16bba96a19afa64914c
adarsh270699 <poddaradarsh27@gmail.com>
2022-07-23 02:13:27 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import img from '../src/javascript_illustration.svg'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-screen min-h-screen text-zinc-600">
+      <div className="h-full flex flex-wrap justify-center content-center">
+        <div className='bg-white shadow-sm hover:bg-gray-600 hover:text-white hover:border-white  hover:border-2 transition duration-500 rounded-xl p-10 space-y-5'>
+          <img className='h-52' src={img} alt='img'></img>
+          <h1 className='font-bold	text-3xl'>Site under construction!</h1>
+        </div>
+      </div>
     </div>
   );
 }
